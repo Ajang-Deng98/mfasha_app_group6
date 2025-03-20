11b1317045a687b7bfe6286bec7a7ddbf0edca7e
buwituze <b.rutagengw@alustudent.com>
2025-03-20 09:31:18 +0200

    Moved Firebase API keys to .env

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'screens/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
